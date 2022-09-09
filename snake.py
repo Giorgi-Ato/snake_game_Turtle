a48bb76d781c09bde5eb7c172b6350708a3cd9cd
Giorgi-Ato <oniani.giorgi.97@gmail.com>
2022-09-09 20:30:48 +0400

    add snake from keyboard control functins

--- snake.py
+++ snake.py
@@ -1,11 +1,17 @@
 from turtle import Turtle
 
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
 class Snake:
 
     def __init__(self):
         self.body_segments = []
         self.create_snake()
+        self.head = self.body_segments[0]
 
     def create_snake(self):
         x_position = 0
@@ -22,4 +28,20 @@ class Snake:
             new_x = self.body_segments[seg_number - 1].xcor()
             new_y = self.body_segments[seg_number - 1].ycor()
             self.body_segments[seg_number].goto(new_x, new_y)
-        self.body_segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
